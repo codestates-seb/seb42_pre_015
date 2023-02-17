e75df2faae6246352d17e15ed818a2acc79e6d85
heeyeon-lim <heeyeon9916@gmail.com>
2023-02-17 14:25:27 +0900

    [init]기본툴세팅 적용하여 front-dev 생성

---
+++ client/src/GlobalStyles.js
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle` 
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    border: 0;
+    font-family: -apple-system, "system-ui", "Segoe UI Adjusted", "Segoe UI", "Liberations Sans", sans-serif;
+    font-weight: 400; 
+    white-space: nowrap;
+    font-size: 11px; 
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  // 미디어 쿼리
+  // 모바일(), 타블렛, PC
+`;
+
+export default GlobalStyles;
--- client/src/components/newComponent.js
+++
@@ -1,5 +0,0 @@
-const newComponent = () => {
-  return <div className="app"></div>;
-};
-
-export default newComponent;
