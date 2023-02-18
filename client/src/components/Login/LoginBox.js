d5bf813b526656d56f3de621d7e7ea062c010ebf
HooniKim <tpgns2289@naver.com>
2023-02-19 00:46:05 +0900

    [style] grid 및 box 고정값 수정

--- client/src/components/Login/LoginBox.js
+++ client/src/components/Login/LoginBox.js
@@ -4,21 +4,34 @@ import LoginButton from '../Buttons';
 const BoxStyle = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: repeat(4, 1fr);
-
-  .formContainer {
-    background-color: white;
+  place-items: center;
+  padding: 24px;
+  .LoginBox {
     display: grid;
     grid-column-start: 2;
-    grid-row-start: 3;
+    grid-template-rows: repeat(3, 1fr);
+  }
+  .formContainer {
+    background-color: white;
     padding: 24px;
-    width: 240px;
-    height: 188px;
+    grid-row-start: 2;
+    width: 307.09px;
+    height: 283.56px;
   }
   .loginForm {
     display: grid;
-    grid-template-rows: repeat(4, 1fr);
-    padding-bottom: 10px;
+    grid-template-rows: 0.5fr 1fr 0.5fr 1fr 0.6fr;
+    row-gap: 2px;
+    height: 100%;
+  }
+  h1 {
+    font-size: 1.25rem;
+    font-weight: bold;
+  }
+  .login--text {
+    border: 1px solid black;
+    border-radius: 5px;
+    height: 33px;
   }
 `;
 
@@ -28,19 +41,19 @@ const LoginBox = () => {
   };
   return (
     <>
-      <div className='LoginBox'>
-        <BoxStyle>
+      <BoxStyle>
+        <div className='LoginBox'>
           <div className='formContainer'>
             <div className='loginForm'>
-              <div>Email</div>
-              <input type='Email' />
-              <div>Password</div>
-              <input type='Password' />
+              <h1>Email</h1>
+              <input type='Email' className='login--text' />
+              <h1>Password</h1>
+              <input type='Password' className='login--text' />
+              <LoginButton className='login--button' onClick={handleClick} />
             </div>
-            <LoginButton className='login-button' onClick={handleClick} />
           </div>
-        </BoxStyle>
-      </div>
+        </div>
+      </BoxStyle>
     </>
   );
 };
