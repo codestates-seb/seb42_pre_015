f92a2020d4323d6cfa499cd95c5be784d53a1bd0
HooniKim <tpgns2289@naver.com>
2023-02-20 00:58:58 +0900

    [refactor] LoginBox의 styled component 분리

--- client/src/components/Login/LoginStyle.js
+++ client/src/components/Login/LoginStyle.js
@@ -5,11 +5,32 @@ const BoxStyle = styled.div`
   grid-template-columns: repeat(3, 1fr);
   place-items: center;
   padding: 24px;
+  margin-top: 70px;
 
   .login-box {
     display: grid;
     grid-column-start: 2;
     grid-template-rows: repeat(3, 1fr);
+    row-gap: 20px;
+    .all-oauth-box {
+      display: grid;
+      grid-template-rows: repeat(2, 1fr);
+    }
+    .oauth-box {
+      display: grid;
+      grid-template-rows: repeat(3, 1fr);
+      grid-row-start: 2;
+      row-gap: 5px;
+    }
+    .logo-box {
+      display: grid;
+      place-items: center;
+      grid-template-rows: repeat(2, 1fr);
+      .logo {
+        grid-row-start: 2;
+        cursor: pointer;
+      }
+    }
   }
   .Container {
     background-color: white;
@@ -20,7 +41,7 @@ const BoxStyle = styled.div`
     box-shadow: 0 1px 4px rgba(0, 0, 0, 0.3);
     border-radius: 5px;
   }
-  .login-form {
+  .Container > .login-form {
     display: grid;
     grid-template-rows: repeat(2, 1fr) 0.5fr;
     row-gap: 2px;
