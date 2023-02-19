5545e19703125864ff40d68c928745c42217e698
heeyeon-lim <heeyeon9916@gmail.com>
2023-02-20 00:52:47 +0900

    [init]브랜치 생성

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import GlobalStyles from './GlobalStyles';
+import QDetailPage from './pages/QDetailPage';
 
 function App() {
   return (
     <>
       <GlobalStyles />
+      <QDetailPage />
     </>
   );
 }
---
+++ client/src/pages/QDetailPage.js
@@ -0,0 +1,88 @@
+import styled from 'styled-components';
+
+const Page = styled.div`
+  display: grid;
+  grid-template-rows: 50px 1000px 100px;
+`;
+
+const Main = styled.main`
+  display: flex;
+  justify-content: center;
+  /* grid-template-columns: 164px auto; */
+`;
+
+const Nav = styled.div`
+  width: 164px;
+  height: 180px;
+  position: sticky;
+  padding-top: 10px;
+  background-color: yellow;
+  @media scan and (max-width: 640) {
+    visibility: hidden;
+  }
+`;
+
+const Content = styled.div`
+  display: grid;
+  grid-template-columns: 800px 300px;
+  grid-template-rows: 50px 34px 10px;
+  grid-template-areas:
+    'question-header question-header'
+    'question-info question-info'
+    'mainbar sidebar';
+  > .question-header {
+    grid-area: question-header;
+    background: red;
+  }
+  > .question-info {
+    grid-area: question-info;
+    background: green;
+  }
+  > .mainbar {
+    grid-area: mainbar;
+    background: purple;
+    height: 800px;
+  }
+  > .sidebar {
+    grid-area: sidebar;
+    width: 300px;
+    height: 800px;
+    background: blue;
+  }
+  @media scan and (max-width: 980) {
+    // sidebar 없애기
+    grid-template-areas:
+      'question-header question-header'
+      'question-info question-info'
+      'mainbar mainbar';
+  }
+`;
+
+function QDetailPage() {
+  return (
+    <Page>
+      <div className='header' style={{ background: 'grey' }}>
+        Header
+      </div>
+      <Main className='main'>
+        <Nav className='nav'></Nav>
+        <Content className='content'>
+          <div className='question-header'></div>
+          <div className='question-info'></div>
+          <div className='mainbar'></div>
+          <div className='sidebar'>
+            <img
+              src='https://s0.2mdn.net/simgad/6461253748063549381'
+              alt='advertisement'
+            />
+          </div>
+        </Content>
+      </Main>
+      <div className='footer' style={{ background: 'grey' }}>
+        Footer
+      </div>
+    </Page>
+  );
+}
+
+export default QDetailPage;
