361698480e0677e3d4de2c5e03a38fb0610058cb
heeyeon-lim <heeyeon9916@gmail.com>
2023-02-19 18:34:18 +0900

    [feat]페이지내 메인, 반복 컴포넌트 제작

---
+++ client/src/components/AskQuestion/WritingGoodQBox.js
@@ -0,0 +1,79 @@
+import styled from 'styled-components';
+
+const BoxContainer = styled.div`
+  width: 70%;
+  min-height: 250px;
+  margin-top: 30px;
+  padding: 24px;
+  border: 1px solid #97c8e9;
+  border-radius: 3px;
+  background-color: #e6f2fa;
+  max-width: 850px;
+  color: #33383d;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  > h2 {
+    font-size: 21px;
+    margin-bottom: 10px;
+  }
+  > p {
+    font-size: 15px;
+    flex-wrap: wrap;
+    white-space: normal;
+    margin-bottom: 5px;
+    > a {
+      font-size: 15px;
+      color: #328acf;
+    }
+  }
+  > h5 {
+    font-size: 13px;
+    font-weight: 600;
+    padding-top: 5px;
+    margin: 10px 0px;
+  }
+  > ul {
+    list-style: disc;
+    margin-left: 30px;
+    > li {
+      white-space: normal;
+    }
+  }
+`;
+
+function WritingGoodQBox() {
+  return (
+    <BoxContainer>
+      <h2>Writing a good question</h2>
+      <p>
+        You’re ready to
+        <a href='https://stackoverflow.com/help/how-to-ask'> ask </a> a&nbsp;
+        <a href='https://stackoverflow.com/help/on-topic'>
+          programming-related question&nbsp;
+        </a>
+        and this form will help guide you through the process.
+      </p>
+      <p>
+        Looking to ask a non-programming question? See&nbsp;
+        <a href='https://stackexchange.com/sites#technology-traffic'>
+          the topics here&nbsp;
+        </a>
+        to find a relevant site.
+      </p>
+      <h5>Steps</h5>
+      <ul>
+        <li>Summarize your problem in a one-line title.</li>
+        <li>Describe your problem in more detail.</li>
+        <li>Describe what you tried and what you expected to happen.</li>
+        <li>
+          Add “tags” which help surface your question to members of the
+          community.
+        </li>
+        <li>Review your question and post it to the site.</li>
+      </ul>
+    </BoxContainer>
+  );
+}
+
+export default WritingGoodQBox;
---
+++ client/src/components/AskQuestion/WritingTipBox.js
@@ -0,0 +1,75 @@
+import styled from 'styled-components';
+import { ReactComponent as SpotPencil } from '../../assets/spotPencil.svg';
+
+const BoxContainer = styled.div`
+  max-width: 348px;
+  border: 1px solid #d0d4d7;
+  border-radius: 3px;
+  > .box-heading {
+    background-color: #f7f8f8;
+    height: 44px;
+    border-bottom: 1px solid #d0d4d7;
+    display: flex;
+    align-items: center;
+    > p {
+      font-size: 15px;
+      padding: 12px;
+    }
+  }
+`;
+
+const BoxContentContainer = styled.div`
+  display: flex;
+  margin: 16px;
+  > .box-content {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    margin-left: 8px;
+    > p {
+      font-size: 12px;
+      white-space: normal;
+      margin-bottom: 10px;
+    }
+  }
+`;
+
+function WritingTipBox() {
+  return (
+    <BoxContainer>
+      <div className='box-heading'>
+        <p>Expand on the problem</p>
+      </div>
+      <BoxContentContainer>
+        <div>
+          <SpotPencil />
+        </div>
+        <div className='box-content'>
+          <p>
+            Your title should summarize the problem. You might find that you
+            have a better idea of your title after writing out the rest of the
+            question.
+          </p>
+          <p>
+            Your title should summarize the problem. You might find that you
+            have a better idea of your title after writing out the rest of the
+            question.
+          </p>
+          <p>
+            Your title should summarize the problem. You might find that you
+            have a better idea of your title after writing out the rest of the
+            question. Your title should summarize the problem. You might find
+            that you have a better idea of your title after writing out the rest
+            of the question. Your title should summarize the problem. You might
+            find that you have a better idea of your title after writing out the
+            rest of the question. Your title should summarize the problem. You
+            might find that you have a better idea of your title after writing
+            out the rest of the question.
+          </p>
+        </div>
+      </BoxContentContainer>
+    </BoxContainer>
+  );
+}
+
+export default WritingTipBox;
