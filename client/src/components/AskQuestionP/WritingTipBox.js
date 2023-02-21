7bb6db999f6600f923e958cb9635e608d20ce7aa
heeyeon-lim <heeyeon9916@gmail.com>
2023-02-21 16:38:34 +0900

    [fix]바뀐 파일명에 맞춰 코드 수정

--- client/src/components/AskQuestionP/WritingTipBox.js
+++ client/src/components/AskQuestionP/WritingTipBox.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { SpotPencilSvg } from '../../assets/AskQPage';
+import { SpotPencilSvg } from '../../assets/AskQuestionSVG';
 
 const BoxContainer = styled.div`
   height: max-content;
