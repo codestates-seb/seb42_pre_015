84e110881f5ae08f70d8330105da69f346e8fab8
hanseungwan1 <seungwanhan542@gmail.com>
2023-02-21 21:46:32 +0900

    잠시저장

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
+import Header from './components/common/Header/Header';
 import GlobalStyles from './GlobalStyles';
+import Main from './pages/aw';
 
 function App() {
   return (
     <>
       <GlobalStyles />
+      <Header></Header>
+      <Main />
     </>
   );
 }
