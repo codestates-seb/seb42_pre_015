b480f2173a149610b753b826c88db09374ad4677
heeyeon-lim <heeyeon9916@gmail.com>
2023-02-21 15:40:32 +0900

    [fix]레이아웃 수정 중

--- client/src/pages/Test2.js
+++ client/src/pages/Test2.js
@@ -19,12 +19,18 @@ const Footer = styled.div`
 const Body = styled.div`
   display: flex;
   justify-content: center;
-  width: 100%;
-  /* top: 53px; */
+  width: 100vw;
   margin-top: 53px;
+  > div {
+    min-width: 164px;
+  }
+
+  > main {
+    min-width: 350px;
+  }
 `;
 
-const Main = styled.div`
+const Main = styled.main`
   display: grid;
   grid-template-rows: minmax(45px, auto) minmax(30px, auto) minmax(800px, auto);
   grid-template-areas:
@@ -34,10 +40,12 @@ const Main = styled.div`
   > .title {
     grid-area: title;
     background-color: aqua;
+    border: 3px solid green;
   }
   > .info {
     grid-area: info;
     background-color: lightyellow;
+    border: 3px solid green;
   }
   > .main {
     grid-area: main;
@@ -46,6 +54,10 @@ const Main = styled.div`
       flex-direction: column;
     }
   }
+
+  @media screen and (max-width: 640px) {
+    width: 100%;
+  }
 `;
 
 const Content = styled.div`
@@ -53,22 +65,36 @@ const Content = styled.div`
   background-color: purple;
   // 안에 채워넣으면 min-height 을 max-content로 바꿉니다.
   min-height: 500px;
+  border: 10px solid green;
+  min-width: 30px;
+
+  @media screen and (max-width: 980px) {
+    width: 98%;
+  }
+`;
+
+const SideWrapper = styled.div`
+  min-width: 300px;
+  width: 300px;
+  height: 1000px;
+  background-color: green;
 `;
 const Side = styled.div`
   background-color: lightcoral;
   min-width: 300px;
   // 안에 채워넣으면 min-height 을 max-content로 바꿉니다.
   min-height: 500px;
+  border: 10px solid green;
 
   @media screen and (max-width: 980px) {
-    min-width: 100%;
+    width: 98%;
   }
 `;
 
 const Nav = styled.div`
   //! Nav 컴포넌트를 불러오면 삭제해도 되는 코드입니다.
   background-color: yellow;
-  min-width: 164px;
+  /* min-width: 164px; */
   height: 250px;
   position: sticky;
   top: 53px;
@@ -84,13 +110,17 @@ function Test() {
     <>
       <Header />
       <Body>
-        <Nav />
+        <div className='nav-wrapper'>
+          <Nav />
+        </div>
         <Main>
           <div className='title'>title</div>
           <div className='info'>info</div>
           <div className='main'>
             <Content>Content</Content>
-            <Side>Side</Side>
+            <SideWrapper className='side-wrapper'>
+              <Side />
+            </SideWrapper>
           </div>
         </Main>
       </Body>
