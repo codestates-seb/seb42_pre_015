b4b1a85436b6e84f1ac16863aeeeb65a6674b123
heeyeon-lim <heeyeon9916@gmail.com>
2023-02-21 14:30:05 +0900

    [feat]레이아웃 수정중

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import GlobalStyles from './GlobalStyles';
 // import DetailPage from './pages/DetailPage';
-import Test from './pages/Test2';
+import Test2 from './pages/Test2';
 
 function App() {
   return (
     <>
       <GlobalStyles />
       {/* <DetailPage /> */}
-      <Test />
+      <Test2 />
     </>
   );
 }
--- client/src/pages/Test2.js
+++ client/src/pages/Test2.js
@@ -1,57 +1,81 @@
 import styled from 'styled-components';
 
 const Header = styled.div`
+  //! Header 컴포넌트를 불러오면 삭제해도 되는 코드입니다.
   grid-area: header;
   background-color: blue;
-  position: sticky;
+  position: fixed;
   top: 0px;
   width: 100vw;
   height: 53px;
 `;
 
 const Footer = styled.div`
+  //! Footer 컴포넌트를 불러오면 삭제해도 되는 코드입니다.
   height: 300px;
   background-color: brown;
 `;
 
-const Main = styled.div`
+const Body = styled.div`
   display: flex;
+  justify-content: center;
   width: 100%;
-  margin: 0px 67px;
+  /* top: 53px; */
+  margin-top: 53px;
 `;
 
-const Nav = styled.div`
-  background-color: yellow;
-  width: 164px;
-  height: 250px;
-  position: sticky;
-  top: 53px;
-  /* left: 67px; */
-`;
-
-const Content = styled.div`
+const Main = styled.div`
   display: grid;
   grid-template-rows: minmax(45px, auto) minmax(30px, auto) minmax(800px, auto);
-  grid-template-columns: minmax(auto, 800px) 300px;
   grid-template-areas:
-    'title title'
-    'info info'
-    'main side';
+    'title'
+    'info'
+    'main';
   > .title {
     grid-area: title;
     background-color: aqua;
   }
   > .info {
     grid-area: info;
-    background-color: red;
+    background-color: lightyellow;
   }
   > .main {
     grid-area: main;
-    background-color: green;
+    display: flex;
+    @media screen and (max-width: 980px) {
+      flex-direction: column;
+    }
   }
-  > .side {
-    grid-area: side;
-    background-color: lightcoral;
+`;
+
+const Content = styled.div`
+  width: 700px;
+  background-color: purple;
+  // 안에 채워넣으면 min-height 을 max-content로 바꿉니다.
+  min-height: 500px;
+`;
+const Side = styled.div`
+  background-color: lightcoral;
+  min-width: 300px;
+  // 안에 채워넣으면 min-height 을 max-content로 바꿉니다.
+  min-height: 500px;
+
+  @media screen and (max-width: 980px) {
+    min-width: 100%;
+  }
+`;
+
+const Nav = styled.div`
+  //! Nav 컴포넌트를 불러오면 삭제해도 되는 코드입니다.
+  background-color: yellow;
+  min-width: 164px;
+  height: 250px;
+  position: sticky;
+  top: 53px;
+  border: 10px solid red;
+
+  @media screen and (max-width: 640px) {
+    display: none;
   }
 `;
 
@@ -59,15 +83,17 @@ function Test() {
   return (
     <>
       <Header />
-      <Main>
+      <Body>
         <Nav />
-        <Content>
-          <Title className='title'>title</div>
-          <Info className='info'>info</div>
-          <div className='main'>main</div>
-          <div className='side'>side</div>
-        </Content>
-      </Main>
+        <Main>
+          <div className='title'>title</div>
+          <div className='info'>info</div>
+          <div className='main'>
+            <Content>Content</Content>
+            <Side>Side</Side>
+          </div>
+        </Main>
+      </Body>
       <Footer />
     </>
   );
