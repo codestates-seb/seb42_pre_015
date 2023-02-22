d1534cc96b68ce8a6426584577f6673db1b76fd8
hanseungwan1 <seungwanhan542@gmail.com>
2023-02-22 16:35:54 +0900

    [init] 답변 수정페이지 시작

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
 import GlobalStyles from './GlobalStyles';
-<<<<<<< HEAD
-=======
 import {
   // QestionEditTagNav,
   // QestionEditTitleNav,
   QestionEditBodyNav
 } from './pages/QestionEditNav';
->>>>>>> fa437d450b439d4e0a7792b14cc7c15aa318247b
 
 function App() {
   return (
     <>
       <GlobalStyles />
-<<<<<<< HEAD
-=======
       {/* <QestionEditTagNav />
       <QestionEditTitleNav></QestionEditTitleNav> */}
       <QestionEditBodyNav />
       <div style={{ height: '1000px' }}></div>
->>>>>>> fa437d450b439d4e0a7792b14cc7c15aa318247b
     </>
   );
 }
