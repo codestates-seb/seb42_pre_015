f133cf2f8570dafe94be6a5dee4f9a2b66b10dce
heeyeon-lim <heeyeon9916@gmail.com>
2023-02-22 21:08:49 +0900

    [feat]DetailSvg 아이콘 추가

--- client/src/assets/DetailSvg.js
+++ client/src/assets/DetailSvg.js
@@ -6,6 +6,7 @@ export const ArrowUpSvg = () => {
       width='36'
       height='36'
       viewBox='0 0 36 36'
+      fill='#B1B7BC'
     >
       <path d='M2 25h32L18 9 2 25Z' />
     </svg>
@@ -20,6 +21,7 @@ export const ArrowDownSvg = () => {
       width='36'
       height='36'
       viewBox='0 0 36 36'
+      fill='#B1B7BC'
     >
       <path d='M2 11h32L18 27 2 11Z' />
     </svg>
@@ -48,8 +50,24 @@ export const SaveUnSelectedSvg = () => {
       width='18'
       height='18'
       viewBox='0 0 18 18'
+      fill='#B1B7BC'
     >
       <path d='m9 10.6 4 2.66V3H5v10.26l4-2.66ZM3 17V3c0-1.1.9-2 2-2h8a2 2 0 0 1 2 2v14l-6-4-6 4Z' />
+    </svg>
+  );
+};
+
+export const HistorySvg = () => {
+  return (
+    <svg
+      aria-hidden='true'
+      className='mln2 mr0 svg-icon iconHistory'
+      width='19'
+      height='18'
+      viewBox='0 0 19 18'
+      fill='#B1B7BC'
+    >
+      <path d='M3 9a8 8 0 1 1 3.73 6.77L8.2 14.3A6 6 0 1 0 5 9l3.01-.01-4 4-4-4h3L3 9Zm7-4h1.01L11 9.36l3.22 2.1-.6.93L10 10V5Z' />
     </svg>
   );
 };
