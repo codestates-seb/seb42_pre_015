d8b3118f30107383d972d0c5ff01752f08aab9ac
heeyeon-lim <heeyeon9916@gmail.com>
2023-02-22 11:49:02 +0900

    App.js default코드로 변경

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import GlobalStyles from './GlobalStyles';
-import { GeneralBtn } from './components/Common/Buttons';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <GeneralBtn />
     </>
   );
 }
