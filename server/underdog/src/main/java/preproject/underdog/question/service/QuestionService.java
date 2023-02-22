73350ec977505703c0aed3d7bab9d4a788f44c18
SeungbaeLee <aha224610@gmail.com>
2023-02-22 15:06:13 +0900

    [ANSWER] Controller 구현

--- server/underdog/src/main/java/preproject/underdog/question/service/QuestionService.java
+++ server/underdog/src/main/java/preproject/underdog/question/service/QuestionService.java
@@ -1,18 +1,2 @@
-//package preproject.question.service;
-//
-//import preproject.question.entity.Question;
-//import preproject.question.repository.QuestionRepository;
-//
-//
-//public class QuestionService {
-//    private final QuestionRepository questionRepository;
-//
-//    public QuestionService(QuestionRepository questionRepository) {
-//        this.questionRepository = questionRepository;
-//    }
-//
-//
-//    public Question createQuestion(Question question){
-//        return questionRepository.save(question);
-//    }
-//}
+package preproject.underdog.question.service;public class QuestionService {
+}
