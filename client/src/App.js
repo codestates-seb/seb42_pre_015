3a2fcdd1b5f31216397917e20d809b6a1b2914f0
hanseungwan1 <seungwanhan542@gmail.com>
2023-02-23 11:30:27 +0900

    [fix] app.js 비움

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
-import Header from './components/common/Header/Header';
 import GlobalStyles from './GlobalStyles';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Header />
     </>
   );
 }
