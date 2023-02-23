fe21ab89d46cc6e1a63f95b543eadd8e6ba711d2
hanseungwan1 <seungwanhan542@gmail.com>
2023-02-23 11:29:40 +0900

    [fix] 헤더 재사용버튼 사용, 재사용버튼 height, padding 프롭스 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
+import Header from './components/common/Header/Header';
 import GlobalStyles from './GlobalStyles';
 
 function App() {
   return (
     <>
       <GlobalStyles />
+      <Header />
     </>
   );
 }
