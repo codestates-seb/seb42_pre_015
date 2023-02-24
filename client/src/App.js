93dd260d02df4c57945ad4e8d086f9d75d8f15d1
heeyeon-lim <heeyeon9916@gmail.com>
2023-02-24 11:31:03 +0900

    [feat]Router 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,39 @@
 import GlobalStyles from './GlobalStyles';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainP from '../src/pages/MainP';
+import AskQuestionP from '../src/pages/AskQuestionP';
+import AnswerEditP from '../src/pages/AnswerEditP';
+import QuestionEditP from '../src/pages/QuestionEditP';
+import DetailP from '../src/pages/DetailPage';
+import LoginP from '../src/pages/LoginP';
+import SignupP from '../src/pages/SignupP';
 
 function App() {
   return (
     <>
       <GlobalStyles />
+      <BrowserRouter>
+        <div className='app'>
+          <Routes>
+            <Route exact path='/' element={<MainP />} />
+
+            <Route exact path='/ask' element={<AskQuestionP />} />
+            <Route
+              exact
+              path='/question/:questionId/answeredit/:answerId'
+              element={<AnswerEditP />}
+            />
+            <Route
+              exact
+              path='/question/:questionId/questionedit'
+              element={<QuestionEditP />}
+            />
+            <Route exact path='/question/:questionId' element={<DetailP />} />
+            <Route exact path='/login' element={<LoginP />} />
+            <Route path='/signup' element={<SignupP />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
