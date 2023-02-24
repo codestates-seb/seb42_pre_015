c26c0df72c47bca004a5298c937a9f6594d32c50
HooniKim <tpgns2289@naver.com>
2023-02-24 10:37:19 +0900

    [docs] app.js 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,11 @@
 import GlobalStyles from './GlobalStyles';
-import LoginP from './pages/LoginP';
-import SignupP from './pages/SignupP';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <BrowserRouter>
-        <Routes>
-          <Route path='/login' element={<LoginP />} />
-          <Route path='/signup' element={<SignupP />} />
-        </Routes>
-      </BrowserRouter>
     </>
   );
 }
+
 export default App;
