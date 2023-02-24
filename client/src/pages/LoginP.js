60a6d9d7112050ef7a1deb9fc43bf2e781b1efc7
HooniKim <tpgns2289@naver.com>
2023-02-24 14:02:14 +0900

    [refactor] 로그인 페이지 리펙토링

--- client/src/pages/LoginP.js
+++ client/src/pages/LoginP.js
@@ -1,6 +1,6 @@
 import LoginBox from '../components/common/Login/LoginBox';
 import styled from 'styled-components';
-import Footer from '../components/common/Footer';
+import Header from '../components/common/Header/Header';
 
 const LoginPageStyle = styled.div`
   background-color: #f1f2f3;
@@ -16,11 +16,11 @@ const LoginPage = () => {
   return (
     <>
       <LoginPageStyle>
+        <Header />
         <div className='login-page'>
           <LoginBox />
         </div>
       </LoginPageStyle>
-      <Footer />
     </>
   );
 };
