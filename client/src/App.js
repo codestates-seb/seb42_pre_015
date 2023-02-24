28837ac93edb8b38db9b27a92697f5fb2a4335df
heeyeon-lim <heeyeon9916@gmail.com>
2023-02-24 10:10:32 +0900

    merge 준비 완료

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,9 @@
 import GlobalStyles from './GlobalStyles';
-import DetailPage from './pages/DetailPage';
-// import Test3 from './pages/Test3';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <DetailPage />
-      {/* <Test3 /> */}
     </>
   );
 }
